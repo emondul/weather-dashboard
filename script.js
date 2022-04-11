4deba7e12955cad2933a66b248d084b6f961407b
Emma Mondul <emondul@gmail.com>
2022-04-10 19:46:19 -0700

    event and fetch functions

--- script.js
+++ script.js
@@ -1,94 +1,43 @@
-// print/render the weather data to the page
-// fetch geo data (lat, lon)
 // my global variables 
-var apikey = "241f26f1238284a5c714bee64572d133";
-var apiurl = "https://api.openweathermap.org";
-var cityHistory = [];
+// apikey = "241f26f1238284a5c714bee64572d133";
 
-// some DOM element references
-    // search bar with a button 
-    // forecast
-    // history
-var historysearch = document.querySelector('#history')
-var form = document.querySelector('#search-form');
-var input = document.querySelector('#search-input');
-var today = document.querySelector('#today');
-var forecast = document.querySelector('#forecast');
-
-// some kind of function that displays searches that the user does 
-
-function showSearch() {
-    historysearch.innerHTML =' ';
-    for( var i = cityHistory.length-1; i >= 0; i--) {
-        var btn = document.createElement('button');
-        btn.setAttribute('type', 'button')
-        btn.setAttribute('area-controls', 'forecast')
-        btn.classList.add('citybtn', 'btncity')
-        btn.setAttribute('search', cityHistory[i])
-        btn.textContent = cityHistory[i]
-        historysearch.append(btn)
-
-    }
-}
-
-// another funtion to use local storage so that displayed cities show up 
-    // setItem
-function localHistory(search){
-    // need to od a setItem 
-    cityHistory.push(search)
-    localStorage.setItem('search-history', JSON.stringify(cityHistory))
-    showSearch()
-}
-// then another function to retrieve data from local storage 
-    // getItem 
-function getlocalStorage(){
-    var data = localStorage.getItem('search-history')
-    if(data){
-        cityHistory = JSON.parse(data)
-    }
-    showSearch()
-}
-
-// a function to the fetch 
-    // we'll need to dynamically create elements and have them filled 
+var search = $('#search');
+var cityName = $('#cityName');
+var todaysWeather = $('#todaysWeather');
+var forecast = $('#forecast');
+var forecastTitle = $('#forecastTitle');
+var searchHistory = $('#searchHistory');
 
 
-// a function to show the forecast section when a city is searched 
+search.on('click', function(){
+    todaysWeather.html('');
+    forecast.html('');
+    forecastTitle.html('');
+    geoData(cityName.val())
+    previousSearchButtons();
+})
 
-function getItems(city, data) {
+searchHistory.on('click',function(event){
+    todaysWeather.html('');
+    forecast.html('');
+    forecastTitle.html('');
+    var previousSearch = event.target.innerText;
+    geoData(previousSearch);
+})
 
-}
+function geoData (city) {
 
-function fetchMyWeather(location) {
-    var { lat} = location;
-    var { lon } = location;
-    var city = location.name;
-    var queryURL = `${apiurl}/data/2.5/onecall?lat=${lat}&${lon}&units=imperial&exclude=minutely,hourly&appid=${apikey}`;
+    var url = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=241f26f1238284a5c714bee64572d133";
 
-    fetch(queryURL)
-    .then(function(res) {
-        return res.json();
-    }).then(function(data) {
-        renderCities(city,data)
-    }).catch(function(err) {
-        console.error(err)
-    });
-}
+    fetch(url) 
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data) {
 
+            console.log(data);
 
-function fetchMyCoords(search) {
-    var queryUrl = `${apiurl}/geo/1.0/direct?q=${search}&limit=5&appid=${apikey}`;
-    fetch(queryUrl)
-    .then(function(res) {
-        return res.json();
-    }).then(function(data){
-        if(!data[0]) {
-            alert('Not found')
-        } else {
-           localHistory(search)
-            fetchMyWeather()
-        }
-    }).catch(function(err) {
-        console.error(err)
-}) 
+            fetchOneCall(data[0].lat, data[0].lon, city);
+        })
+
 }
